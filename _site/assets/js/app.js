5370aa18a5f2bf8bc863b5cf1147626158608226
“garethjohnsdesign“ <info@garethjohnsdesign.com>
2016-03-21 20:10:34 +0000

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -63,3 +63,17 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
 });
+
+
+$(document).ready(function () {
+ wow = new WOW(
+      {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       200,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+    );
+    wow.init();
+});
